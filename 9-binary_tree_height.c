7b767b9a7ac401789b51afe4417ed276f52dc5a5
Rcastro89 <r_castro_m@yahoo.es>
2022-03-23 23:09:05 -0500

    9 find

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+/**
+ * find_max - Functiont that say if a node exist.
+ * @left: number of brothers left.
+ * @right: number of brothers right.
+ * Return: side with more siblings.
+ */
+size_t max(int left, int right)
+{
+	if (left >= right)
+		return (left);
+	return (right);
+}
+
+/**
+ * find_height - Recursive function that count returns the length
+ * of the longest branch of a tree
+ * @tree: Parent node.
+ * Return: Heigth of single nodes plus one.
+ */
+size_t height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (max(height(tree->left),
+			 height(tree->right)) + 1);
+}
+/**
+ * binary_tree_height - Function that measures the height of a binary tree
+ * using in-order traversal.
+ * @tree: Is a pointer to the root node or any other node.
+ * Return: Heigh of binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t res = 0;
+
+	res = height(tree);
+	if (res == 0)
+		return (res);
+	return (res - 1);
+}
